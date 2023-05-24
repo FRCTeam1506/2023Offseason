cfe9de4275c9c9f8a7eb619d2121eb13525fa65f
FRCTeam1506 <frcteam1506@yahoo.com>
2023-05-23 20:57:35 -0400

    mas

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,12 +4,23 @@ import frc.robot.Constants;
 import frc.lib.math.Conversions;
 
 
-import com.ctre.phoenix.motorcontrol.ControlFrame;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+// import com.ctre.phoenix.motorcontrol.ControlFrame;
+// import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+// import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+// import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+// import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
+import com.ctre.phoenix6.controls.jni.ControlJNI;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
@@ -19,23 +30,34 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenixpro.unmanaged.Unmanaged;
+
+
 public class IntakeSubsystem extends SubsystemBase {
 
     Conversions conversions = new Conversions();
 
     private DoubleSolenoid solenoid;
     private TalonFX motor = new TalonFX(Constants.IntakeSubsystem.MOTOR_ID);
-    double encoderCount = motor.getSelectedSensorPosition();
+    double encoderCount = motor.getPosition().getValue();
 
 
     public IntakeSubsystem (PneumaticHub hub) {
         solenoid = hub.makeDoubleSolenoid(Constants.IntakeSubsystem.SolenoidId1, Constants.IntakeSubsystem.SolenoidId2);
-        motor.configFactoryDefault();
-        motor.setControlFramePeriod(ControlFrame.Control_3_General, 100);
-        motor.setInverted(TalonFXInvertType.CounterClockwise);
+        motor.getConfigurator().apply(new TalonFXConfiguration());
+        //could not convert to pro - next line
+        // motor.setControlFramePeriod(ControlJNI.Control_3_General, 100);
+        motor.setInverted(false);
         
         //current limit burn out the motor at states
         TalonFXConfiguration intakeMotorConfig   = new TalonFXConfiguration();
+        motor.CurrentLimits.StatorCurrentLimitEnable = false;
+
         SupplyCurrentLimitConfiguration driveSupplyLimit = new SupplyCurrentLimitConfiguration(
             Constants.SwerveDrivetrain.DRIVE_ENABLE_CURRENT_LIMIT, 
             Constants.SwerveDrivetrain.DRIVE_CONTINUOUS_CL, 
